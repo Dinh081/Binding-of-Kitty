e98f9879f8dd1b6bf0d61a1cd27e2d83a160729c
Dinh081 <C_truong@web.de>
2025-01-16 11:24:00 +0100

    KI and invincible frames

--- Binding of Kitty/Assets/Scripts/EnemyBehavior.cs
+++ Binding of Kitty/Assets/Scripts/EnemyBehavior.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEngine.GraphicsBuffer;
 
 public class EnemyBehavior : MonoBehaviour
 {
@@ -9,6 +10,12 @@ public class EnemyBehavior : MonoBehaviour
     [SerializeField] private UIUpdater _UI;
     private Rigidbody2D _rb;
     public GameHandler gameHandler;
+    private Transform _target;
+    [SerializeField] private float _speed = 1f;
+    private float _fleeDuration = 0.8f;  // Time the enemy moves away
+    [SerializeField] private float _fleeSpeed = 2f;  // Speed at which the enemy moves away
+
+    private bool _isFleeing = false;  // Flag to check if the enemy is currently fleeing
 
 
     private void Awake()
@@ -19,11 +26,13 @@ public class EnemyBehavior : MonoBehaviour
     void Start()
     {
         _UI.UpdateEnemyHealth(GetHealth());
+        _target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
     public void DecreaseHealth(int amount)
     {
         _enemyHealth -= amount;  // Reduce health by the specified amount
         gameHandler.OnHealthChanged(GetHealth());
+        _UI.UpdateEnemyHealth(GetHealth());
     }
 
     public int GetHealth()
@@ -32,8 +41,45 @@ public class EnemyBehavior : MonoBehaviour
     }
 
     // Update is called once per frame
+
     void Update()
     {
+
+        // If the enemy is fleeing, move away from the player
+        if (_isFleeing)
+        {
+            // Calculate direction away from the player
+            Vector2 awayFromPlayer = (transform.position - _target.position).normalized;
+
+            // Move the enemy away from the player (move towards a position offset from current position)
+            transform.position = Vector2.MoveTowards(transform.position, (Vector2)transform.position + awayFromPlayer, _fleeSpeed * Time.deltaTime);
+        }
+        else
+        {
+            // Otherwise, move toward the player
+            transform.position = Vector2.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
+        }
+
         _UI.UpdateEnemyHealth(_enemyHealth);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Start fleeing behavior for a short duration
+            StartCoroutine(FleeFromPlayer());
+        }
+    }
+
+    private IEnumerator FleeFromPlayer()
+    {
+        // Set the fleeing flag to true
+        _isFleeing = true;
+
+        // Wait for the specified duration while the enemy is fleeing
+        yield return new WaitForSeconds(_fleeDuration);
+
+        // After the duration, stop fleeing and allow normal movement
+        _isFleeing = false;
     }
 }
--- Binding of Kitty/Assets/Scripts/Player.cs
+++ Binding of Kitty/Assets/Scripts/Player.cs
@@ -1,110 +1,111 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] InputActionReference _movementInput;
+    [SerializeField] private InputActionReference _movementInput;
     [SerializeField] private InputActionReference _shootInput;
-    [SerializeField] private GameObject _projectilePrefab; // Reference to the projectile prefab
-    [SerializeField] private Transform _shootPoint; // Point where the projectile spawns
-    [SerializeField] private float _shootForce = 10f; // Force to apply to the projectile
-    private Vector2 _lastDirection = Vector2.left; // Default direction (facing left)
+    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private Transform _shootPoint;
+    [SerializeField] private float _shootForce = 10f;
+    private Vector2 _lastDirection = Vector2.left;
     private Vector2 _movement;
     private Rigidbody2D _rb;
     [SerializeField] private float _speed;
     [SerializeField] private int _health;
     [SerializeField] private UIUpdater _UI;
+    private bool _isInvincible = false;
+    private float _invincibilityDuration = 0.5f;
+
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
-
     }
+
     private void OnEnable()
     {
         _movementInput.action.Enable();
-        _shootInput.action.Enable(); // Enable shooting input
+        _shootInput.action.Enable();
     }
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
         _UI.UpdateHealth(_health);
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         _movement = _movementInput.action.ReadValue<Vector2>();
-
 
         if (_movement != Vector2.zero)
         {
             _lastDirection = _movement.normalized;
         }
 
-        // Shoot in the last direction when the shoot button is pressed
         if (_shootInput.action.triggered)
         {
             ShootProjectile(_lastDirection);
         }
-        _UI.UpdateHealth(_health);
-
     }
 
     private void FixedUpdate()
     {
         _rb.velocity = _movement * _speed;
-
     }
+
     private void ShootProjectile(Vector2 direction)
     {
         if (_projectilePrefab && _shootPoint)
         {
-            if (_projectilePrefab && _shootPoint)
+            GameObject projectile = Instantiate(_projectilePrefab, _shootPoint.position, Quaternion.identity);
+            Rigidbody2D projRb = projectile.GetComponent<Rigidbody2D>();
+
+            if (projRb != null)
             {
-                GameObject projectile = Instantiate(_projectilePrefab, _shootPoint.position, Quaternion.identity);
-                Rigidbody2D projRb = projectile.GetComponent<Rigidbody2D>();
-
-                if (projRb != null)
+                if (direction == Vector2.zero)
                 {
-                    if (direction == Vector2.zero)
-                    {
-                        direction = _lastDirection;
-                    }
-
-                    projRb.velocity = direction.normalized * _shootForce; // Shoot in the last known direction
+                    direction = _lastDirection;
                 }
 
+                projRb.velocity = direction.normalized * _shootForce;
             }
-
         }
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Enemy"))
-            if (_health < 2)
+        if (collision.gameObject.CompareTag("Enemy") && !_isInvincible)
+        {
+            _health--;
+            _UI.UpdateHealth(_health);
+
+            if (_health <= 0)
             {
                 SceneManager.LoadScene(SceneManager.GetActiveScene().name);
             }
             else
             {
-                _health--;
-                _UI.UpdateHealth(_health);
-
+                StartCoroutine(InvincibilityCoroutine());
             }
-        else if (collision.gameObject.CompareTag("Door")) {
+        }
+        else if (collision.gameObject.CompareTag("Door"))
+        {
             SceneManager.LoadScene("Level2");
         }
+    }
+
+    private IEnumerator InvincibilityCoroutine()
+    {
+        _isInvincible = true;
+        yield return new WaitForSeconds(_invincibilityDuration);
+        _isInvincible = false;
     }
 
     private void OnDisable()
     {
         _movementInput.action.Disable();
-        _shootInput.action.Disable(); // Disable shooting input
+        _shootInput.action.Disable();
     }
-
 }
-
